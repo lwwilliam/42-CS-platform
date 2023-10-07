05268d0996aeef9a464fb56bb061d2c5222fc192
lwwilliam <lee1052william@gmail.com>
2023-10-08 03:19:32 +0800

    added embedding posts for feed

--- src/Feed/Feed.jsx
+++ src/Feed/Feed.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import { RightSideContainer } from '../Components/Navbar/NavbarElements';
+import "react-slideshow-image/dist/styles.css";
+import "./Feed.css";
+import { InstagramEmbed } from 'react-social-media-embed';
 
 function Feed() {
+
   return (
     <div>
       <Navbar />
-        <RightSideContainer>
-          <h1>Your Feed</h1>
-        </RightSideContainer>
+      <RightSideContainer>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <InstagramEmbed url="https://www.instagram.com/p/Ctp859gPSot/" width={800}/>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <InstagramEmbed url="https://www.instagram.com/p/CyBVtRAvymR/" width={800}/>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <InstagramEmbed url="https://www.instagram.com/p/Cx7diC1vZP_/" width={800}/>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <InstagramEmbed url="https://www.instagram.com/p/CsydDN7PbOG/" width={800}/>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <InstagramEmbed url="https://www.instagram.com/p/CsVXYe3pBiG/" width={800}/>
+      </div>
+      </RightSideContainer>
     </div>
   );
 }
