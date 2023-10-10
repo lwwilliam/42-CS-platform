2fea20d902923e5c21aba8edddfb9563aa8276a6
WardenOTD <107560435+WardenOTD@users.noreply.github.com>
2023-10-10 10:24:01 +0000

    restructured cliubsinfo page and ui/ux, pending dynamic changes

--- src/ClubInfo/ClubInfo.jsx
+++ src/ClubInfo/ClubInfo.jsx
@@ -19,7 +19,7 @@ function ClubInfo() {
   function redirSignUp(path) {
     navigate('/SignUp/' + path);
   };
-  
+
   useEffect(() => {
     setClubs(clubsData.Clubs); // Use the imported JSON data directly
   }, []);
@@ -31,25 +31,42 @@ function ClubInfo() {
       <RightSideContainer>
         <div className='club-header'>Clubs</div>
         <div style={{marginTop: "4vw"}}></div>
-        <Accordion>
-          {clubs.map((club, index) => (
-            <AccordionPanel key={index}>
-              <AccordionTitle>
-                <div className="text-2xl">{club.Name}</div>
-              </AccordionTitle>
-              <AccordionContent>
-                <div className="text-2xl text-justify bg-gray-500/20 ">{club.Description}</div>
-                <div class="place-items-center text-center p-2">
-                  <button onClick={() => redirSignUp(club.Name)} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 h-10 w-28 rounded-full">
-                    Sign up
-                  </button>
-                </div>
-              </AccordionContent>
-            </AccordionPanel>
-          ))}
-        </Accordion>
+        <div>
+          <Accordion collapseAll className='border-[#2eb6d1] divide-[#2eb6d1] w-[60%]'>
+            {/* change input here for club category names */}
+            {/* clubs.map and parameter before index */}
+            {clubs.map((index) => (
+              <AccordionPanel key={index}>
+                <AccordionTitle className="bg-blue-950 text-white hover:bg-blue-900 hover:text-white focus:bg-blue-950 h-[9vh]">
+                  {/* change display to club category names */}
+                  <div className="text-[2.2vw]">Clubs Category</div>
+                </AccordionTitle>
+                <AccordionContent className='bg-slate-700'>
+                  <Accordion collapseAll className='border-[#2eb6d1] divide-[#2eb6d1]'>
+                      {clubs.map((club, index) => (
+                        <AccordionPanel key={index}>
+                          <AccordionTitle className="bg-blue-950 text-white hover:bg-blue-900 hover:text-white focus:bg-blue-950">
+                            <div className="text-2xl">{club.Name}</div>
+                          </AccordionTitle>
+                          <AccordionContent className='bg-slate-700 text-blue-300'>
+                            <div className="text-2xl text-justify">{club.Description}</div>
+                            <div class="place-items-center text-center p-2">
+                              <button onClick={() => redirSignUp(club.Name)} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 h-10 w-28 rounded-full">
+                                Sign up
+                              </button>
+                            </div>
+                          </AccordionContent>
+                        </AccordionPanel>
+                      ))}
+                    </Accordion>
+                </AccordionContent>
+              </AccordionPanel>
+            ))}
+          </Accordion>
+        </div>
       </RightSideContainer>
     </div>
+
   );
 }
 
