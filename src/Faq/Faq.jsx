715c771275b85ce9b9696d9b74f19ba1a49b6667
laian <tanlaian2004@gmail.com>
2024-01-26 18:58:36 +0800

    fix: footer responsive

--- src/Faq/Faq.jsx
+++ src/Faq/Faq.jsx
@@ -6,18 +6,24 @@ import Layout from '../Components/layout';
 
 function FAQ() {
 
-  return (
-    <Layout>
-      <div className='mx-auto w-[80%] py-5'>
-			<div className='font-poppins font-bold md:text-5xl text-4xl md:mb-auto mb-6'>Frequently Asked Questions</div>
-        {json.map((edge) => (
-          <div key={edge} className='px-7 py-2'>
-            <div className='text-[1.2vw] font-bold '>{edge.Question}</div>
-            <div className='text-[1vw] pl-4 '>{edge.Answer}</div>
-          </div>
-        ))}
-      </div>
-    </Layout>
+	return (
+		<>
+			<Layout>
+				<div className="w-full flex flex-col justify-center items-center">
+					<div className='w-[80%] py-8 md:py-16 items-center'>
+						<div className='font-poppins font-bold md:text-5xl text-4xl md:mb-auto mb-6'>Frequently Asked Questions</div>
+							<div className='pt-10'>
+								{json.map((edge) => (
+									<div key={edge} className='px-7 py-2'>
+										<div className='text-[1.2vw] font-bold '>{edge.Question}</div>
+										<div className='text-[1vw] pl-4 '>{edge.Answer}</div>
+									</div>
+								))}
+							</div>
+					</div>
+				</div>
+			</Layout>
+		</>
   );
 }
 
