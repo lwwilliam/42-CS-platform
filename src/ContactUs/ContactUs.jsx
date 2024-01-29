52b4abd656a0982c3dd0f1001f62e599b1daa7e4
laian <tanlaian2004@gmail.com>
2024-01-29 17:59:35 +0800

    feat: responsive

--- src/ContactUs/ContactUs.jsx
+++ src/ContactUs/ContactUs.jsx
@@ -15,7 +15,7 @@ function ContactUs()
 							<div className='font-poppins font-bold md:text-5xl text-4xl md:mb-auto mb-6'>Contact Us</div>
 							<div className="pb-5">
 								<div className="text-3xl font-poppins font-bold md:py-6 py-4">Non-sports Clubs & Societies</div>
-								<div className="grid grid-cols-3 gap-5 pt-5">
+								<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pt-5">
 									<ContactCard name='Ms. Shazrina' email='shazrinar@sunway.edu.my'/>
 									<ContactCard name='Ms. Valerie' email='valeriek@sunway.edu.my'/>
 									<ContactCard name='Mr. Nazmi' email='nazmi@sunway.edu.my'/>
@@ -23,7 +23,7 @@ function ContactUs()
 							</div>
 							<div className="pb-5">
 								<div className="text-3xl font-poppins font-bold md:py-6 py-4">Sports Clubs & Societies</div>
-								<div className="grid grid-cols-3 gap-5 pt-5">
+								<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pt-5">
 									<ContactCard name='Mr. Mok' email='ccmok@sunway.edu.my'/>
 									<ContactCard name='Mr. Jeremy' email='jeremyk@sunway.edu.my'/>
 									<ContactCard name='Ms. Hajar' email='sitihajarm@sunway.edu.my'/>
@@ -31,7 +31,7 @@ function ContactUs()
 							</div>
 							<div className="pb-5">
 								<div className="text-3xl font-poppins font-bold md:py-6 py-4">General</div>
-								<div className="grid grid-cols-3 gap-5 pt-5">
+								<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pt-5">
 									<ContactCard name='Sunway University Student Council' email='ccmok@sunway.edu.my'/>
 									<ContactCard name='Sunway College Student Council' email='scc.info@imail.sunway.edu.my'/>
 									<ContactCard name='Student LIFE Office' email='-'/>
